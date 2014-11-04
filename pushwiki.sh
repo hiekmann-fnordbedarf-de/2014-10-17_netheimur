2bbcfe992120d647d4d66fdf4759ed601823c906
one <one@charlie>
2014-11-04 09:49:22 +0000

    resolved chowning root

--- pushwiki.sh
+++ pushwiki.sh
@@ -1,25 +1,29 @@
 #! /bin/bash
-pw1="${1}"
-pw2="${2}"
-pw3="${3}"
-printf "Temp Folder "
-tmpfolder=$(mktemp --directory ~/dokuwiki.XXX)
-printf "${tmpfolder}\n"
+pw1="${1}" # encryption password
+pw2="${2}" # pgp key password
+pw3="${3}" # owncloud user:password @ owncloud server
+
+# files
+mytar="/home/one/Downloads/dokuwiki_$(date +%Y-%m-%d).tar.gz"
+mykey="0x823AA1C0"
+
 printf "Copying DokuWiki\n"
-cp --recursive /usr/share/dokuwiki ${tmpfolder}
-printf "Naming backup "
-backupname="dokuwiki_""$(date +%Y-%m-%d)"".tar.gz.gpg.sig"
-printf "${backupname}\n"
-printf "Compressing DokuWiki\n"
-tar --gzip --create --absolute-names --file ${tmpfolder}/dokuwiki.tar.gz ${tmpfolder}/dokuwiki
-printf "Symmetric encrypting of DokuWiki\n"
-gpg --no-use-agent --cipher-algo AES256 --symmetric --passphrase ${pw1} --output ${tmpfolder}/dokuwiki.tar.gz.gpg ${tmpfolder}/dokuwiki.tar.gz 
-printf "Signing encrypted DokuWiki\n"
-gpg --no-tty --quiet --no-use-agent --output ${tmpfolder}/dokuwiki.tar.gz.gpg.sig --passphrase ${pw2} --local-user 0x57E5F195 --sign ${tmpfolder}/dokuwiki.tar.gz.gpg &> /dev/null
+cd /var/lib/
+sudo tar --create --same-owner --same-permissions --file "${mytar}" dokuwiki
+sudo chown one:one "${mytar}"
+
+printf "Encrypting DokuWiki\n"
+gpg --no-use-agent --cipher-algo AES256 --symmetric --passphrase ${pw1} --output ${mytar}.gpg ${mytar} 
+
+printf "Signing DokuWiki\n"
+gpg --no-tty --quiet --no-use-agent --output ${mytar}.gpg.sig --passphrase ${pw2} --local-user "${mykey}" --sign ${mytar}.gpg # &> /dev/null
+
 printf "Pushing to owncloud\n"
-mv ${tmpfolder}/dokuwiki.tar.gz.gpg.sig ${tmpfolder}/${backupname}
-cp ${tmpfolder}/${backupname} ~/ownCloud/dokuwiki/
-owncloudcmd ~/ownCloud https://${pw3}/remote.php/webdav/  > /dev/null 
-printf "Cleaning up"
-trap "{ rm -rf ${tmpfolder} ; exit 255; }" EXIT
-printf "\nIn order to verify: gpg --verify [file]\nIn order to decrypt: gpg --output [resulting file] --decrypt [file]\n"
+cp ${mytar}.gpg.sig ~/ownCloud/dokuwiki/
+owncloudcmd ~/ownCloud owncloud://${pw3}/remote.php/webdav/ &> /dev/null 
+
+
+printf "Cleaning up\n"
+trap "{ rm -rf ${mytar}* ; exit 255; }" EXIT
+
+printf "\nVerfication of public key: gpg --verify [file]\nDecryption: gpg --output [resulting file] --decrypt [file]\nRestoring DokuWiki: replace dokuwiki folder with decrypted one. Set permissions\n"
